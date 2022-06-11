f43ea83bcbcc1189da1e3d5073464ff2471278b8
Thácio Helmer <thaciohelmer@hotmail.com>
2022-06-11 09:59:32 -0300

    Atualização 3

--- curso_web_completo/JavaScript/Objeto/CriandoObjetos.js
+++ curso_web_completo/JavaScript/Objeto/CriandoObjetos.js
@@ -1,3 +1,48 @@
 // Literal
 
+const obj1 = {};
+console.log(obj1);
 
+//Object em js
+const obj2 = new Object();
+console.log(obj2);
+
+// Funçoes construtoras
+function Produto(nome, preco, desc) {
+  this.nome = nome; // This define um atributo publico
+  this.getPrecoComDesconto = () => {
+    return preco * (1 - desc);
+  };
+}
+
+const p1 = new Produto("Caneta", 7.99, 0.15);
+console.log(p1.getPrecoComDesconto());
+
+const p2 = new Produto("Notebook", 2998.99, 0.25);
+console.log(p2.getPrecoComDesconto());
+
+// Funcão Factory
+function criarFuncionario(nome, salarioBase, faltas) {
+  return {
+    nome,
+    salarioBase,
+    faltas,
+    getSalario() {
+      return (salarioBase / 30) * (30 - faltas);
+    },
+  };
+}
+
+const f1 = criarFuncionario("Joao", 7980, 4);
+const f2 = criarFuncionario("Maria", 11400, 1);
+console.log(f1.getSalario(), f2.getSalario());
+
+// Object.create
+const filha = Object.create(null);
+
+filha.nome = "Ana";
+console.log(filha);
+
+// Função famosa que retorna um objeto
+const fromJSON = JSON.parse('{"info": "Sou um JSON"}');
+console.log(fromJSON.info);
